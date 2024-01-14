4db4f18a91cc371b7e74c1fdc6d452c22cfb1e88
Onni <onni.vitikainen@tuni.fi>
2024-01-14 23:26:06 +0200

    Done crud2

--- elm/players_CRUD2/src/Main.elm
+++ elm/players_CRUD2/src/Main.elm
@@ -62,18 +62,46 @@ playersDecoder =
 
 
 fetchPlayers : String -> Cmd Msg
-fetchPlayers url = Cmd.none
+fetchPlayers url =
+    Http.get
+        { url = url
+        , expect = Http.expectJson FetchPlayers playersDecoder
+        }
+
 
 postPlayerReq : String -> Player -> Cmd Msg
-postPlayerReq url player = Cmd.none
+postPlayerReq url player =
+    Http.post
+        { url = url
+        , body = Http.jsonBody (playerEncoder player)
+        , expect = Http.expectJson (\result -> AddPlayer (result)) playerDecoder
+        }
 
 
 deletePlayerReq : String -> Int -> Cmd Msg
-deletePlayerReq url id = Cmd.none
+deletePlayerReq url id = 
+    Http.request
+        { method = "DELETE"
+        , headers = []
+        , url = url ++ String.fromInt id
+        , body = Http.emptyBody
+        , expect = Http.expectWhatever (DeletePlayer id)
+        , timeout = Nothing
+        , tracker = Nothing
+        }
 
 
 putPlayerReq : String -> Player -> Cmd Msg
-putPlayerReq url player = Cmd.none
+putPlayerReq url player =
+    Http.request
+        { method = "PUT"
+        , headers = []
+        , url = url ++ String.fromInt player.id
+        , body = Http.jsonBody (playerEncoder player)
+        , expect = Http.expectJson (\result -> AddPlayer (result)) playerDecoder
+        , timeout = Nothing
+        , tracker = Nothing
+        }
 
 
 listLast : List a -> Maybe a
@@ -105,34 +133,152 @@ init _ =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        SetName word ->
-            ( model, Cmd.none )
+        SetName name ->
+            let 
+                oldPlayer  = model.newPlayer
+                newNewPlayer = 
+                    { oldPlayer | name = name }
+            in 
+                let
+                    updatedModel = { model | newPlayer =  newNewPlayer }
+                in
+                (Debug.log ("Name Changed: " ++ name) updatedModel, Cmd.none)
+
 
         FetchPlayers data ->
-            ( model, Cmd.none )
+            case data of
+                Ok players ->
+                     let
+                        newId =
+                            case List.maximum <| List.map .id players of
+                                Just maxId ->
+                                    maxId + 1
+
+                                Nothing ->
+                                    1
+
+                        updatedModel =
+                            { model | reqStatus = "", players = players, newPlayer = initPlayer newId }
+                    in
+                    Debug.log "fetch players" (updatedModel, Cmd.none)
+
+                Err _ ->
+                    let
+                        updatedModel =
+                            { model | reqStatus = "An error has occurred!!!"}
+                    in
+                    (updatedModel, Cmd.none)
+
 
         PostPlayerReq ->
-            ( model, Cmd.none )
+            (model, postPlayerReq model.baseUrl model.newPlayer)
 
         AddPlayer data ->
-            ( model, Cmd.none )
+            case data of
+                Ok player -> 
+                    let
+                        newId =
+                            case List.maximum <| List.map .id model.players of
+                                Just maxId ->
+                                    maxId + 1
+
+                                Nothing ->
+                                    1
+
+                        updatedModel =
+                            { model |
+                                players = model.players ++ [Player newId player.name player.isActive]
+                                , newPlayer = initPlayer (newId + 1)
+                            }
+                    in
+                    (Debug.log "Players: " updatedModel, Cmd.none)
+
+                Err _ -> 
+                    let
+                        updatedModel = 
+                            { model | reqStatus = "An error has occurred!!!"}
+                    in
+                    (updatedModel, Cmd.none)
+
+
 
         PutPlayerReq id status ->
-            ( model, Cmd.none )
+            let
+                updatedPlayers =
+                    List.map
+                        (\player ->
+                            if player.id == id then
+                                { player | isActive = status }
+                            else
+                                player
+                        )
+                        model.players
+            in
+            ({ model | players = updatedPlayers }, putPlayerReq model.baseUrl { id = id, name = "", isActive = status })
+
 
         ModifyPlayer data ->
-            ( model, Cmd.none )
+            case data of
+                Ok updatedPlayer ->
+                    let
+                        updatedPlayers =
+                            List.map
+                                (\player ->
+                                    if player.id == updatedPlayer.id then
+                                        updatedPlayer
+                                    else
+                                        player
+                                )
+                                model.players
+                    in
+                    ({ model | players = updatedPlayers }, Cmd.none)
+
+                Err _ ->
+                    let
+                        updatedModel = 
+                            { model | reqStatus = "An error has occurred!!!"}
+                    in
+                    (updatedModel, Cmd.none)
 
         DeletePlayerReq id ->
-            ( model, Cmd.none )
+            (model, deletePlayerReq model.baseUrl id)
+
 
         DeletePlayer id data ->
-            ( model, Cmd.none )
+            case data of
+                Ok _ ->
+                    ({ model | players = List.filter (\player -> player.id /= id) model.players }, Cmd.none)
 
+                Err _ ->
+                    let
+                        updatedModel =
+                            { model | reqStatus = "An error has occurred!!!" }
+                    in
+                    (updatedModel, Cmd.none)
 
 view : Model -> Html Msg
 view model =
-    h1 [] [ text "Elm Exercise: Players CRUD2" ]
+    div []
+        [ h1 [] [ text "Elm test Exercise: Players CRUD" ]
+        , Html.form [ id "submit-player", onSubmit PostPlayerReq ]
+            [ input [ type_ "text", id "input-player", value model.newPlayer.name, onInput SetName ] [] 
+            , button [ type_ "submit", id "btn-add"] [ text "Add"] ] 
+        , ol [ id "players-list" ] (List.map viewPlayer model.players)
+        , div [id "request-status"] [text model.reqStatus]
+        ]
+
+viewPlayer : Player -> Html Msg
+viewPlayer player =
+    li [id ("player-" ++ String.fromInt (player.id))]
+        [ div [class "player-name"] [text player.name]
+        , label []
+            [ input [type_ "checkbox", class "player-status", onCheck (\isChecked -> PutPlayerReq player.id isChecked), checked player.isActive] []
+            , span [class "checkmark"] []
+            , text (if player.isActive then "Active" else "Not active")
+            ]
+        , button [class "btn-delete", onClick (DeletePlayerReq player.id)] [text "Delete"]
+    ]
+
 
 
 main : Program () Model Msg
