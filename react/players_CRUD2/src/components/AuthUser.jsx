aa1c608705ed9da191e8f2695be2140610fa6ce6
Jaakko Rajala <jaakko.rajala@tuni.fi>
2024-02-01 10:02:27 +0200

    Fix problems with instructions/tests

--- react/players_CRUD2/src/components/AuthUser.jsx
+++ react/players_CRUD2/src/components/AuthUser.jsx
@@ -3,8 +3,8 @@
  * Use the given template with props to create a AuthUser component similar to the AuthUser component in the Vue exercise. Instead of using a template, use JSX.
  *
  * isLoggedIn is a prop boolean that indicates if the user is logged in or not.
- * onLogin is a prop function that will be called when the login link is clicked. It should be called with the username and password as arguments.
- * onRegister is a prop function that will be called when the register link is clicked. It should be called with the username and password as arguments.
+ * onLogin is a prop function that will be called when the form is submitted and user is in "login" view. It should be called with the username and password as arguments.
+ * onRegister is a prop function that will be called when the form is submitted and user is in the "register" view. It should be called with the username and password as arguments.
  * onLogout is a prop function that will be called when the logout link is clicked. It should be called with no arguments.
  *
  * REMEMBER: use the same ids, classes and attributes as in the Vue exercise in the same places to pass the tests.
